587fb0294324e9486817ed455a4e4aa2d5e1b0e8
Maxim Salnikov <salnikov@gmail.com>
2020-05-12 00:49:47 +0200

    build updated

--- tools/workbox.js
+++ tools/workbox.js
@@ -2,7 +2,7 @@ const workboxBuild = require('workbox-build');
 
 const buildSW = () =>
   workboxBuild.injectManifest({
-    swSrc: 'themes/devfest-theme-hugo/static/sw.js',
+    swSrc: 'themes/ngvikings-2020/static/sw.js',
     swDest: 'public/sw.js',
     globDirectory: 'public',
     globPatterns: ['**\/*.{html,json,png,xml,ico,txt,svg,jpg,webp,jpeg,ics,js,css}']
